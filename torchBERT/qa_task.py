6becd06ee049b9e0d35dc222b2c3410473263664
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2020-03-11 14:50:48 -0700

    save best f1 model in qa task

--- torchBERT/qa_task.py
+++ torchBERT/qa_task.py
@@ -256,7 +256,7 @@ if __name__ == "__main__":
     lr = args.lr
     optimizer = torch.optim.SGD(model.parameters(), lr=lr)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.1)
-    best_val_loss = None
+    best_val_f1 = None
 
     for epoch in range(1, args.epochs + 1):
         epoch_start_time = time.time()
@@ -269,10 +269,10 @@ if __name__ == "__main__":
                                    val_loss, val_exact, val_f1))
         print('-' * 89)
         # Save the model if the validation loss is the best we've seen so far.
-        if not best_val_loss or val_loss < best_val_loss:
+        if not best_val_f1 or val_f1 > best_val_f1:
             with open(args.save, 'wb') as f:
                 torch.save(model, f)
-            best_val_loss = val_loss
+            best_val_f1 = val_f1
         else:
             scheduler.step()
 
